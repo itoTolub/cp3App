95450c43d178ef9f1c7cf375ee539ac2256500db
itoTolub <ito.tolub@yahoo.com>
2018-11-29 10:17:04 +0700

    second commit

--- app/src/main/java/com/example/burfitzam/kalkulator/MainActivity.java
+++ app/src/main/java/com/example/burfitzam/kalkulator/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button button0,button1,button2,button3,button4,button5,button6,button7,button8,button9;
     Button buttonTambah,buttonKurang,buttonKali,buttonBagi,buttonSamaDengan;
     Button buttonClear;
-
+//fitr0 firmansyah
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
